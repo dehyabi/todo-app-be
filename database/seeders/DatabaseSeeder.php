dc83efd3c5de8b312a4bbc15d95af30b11683423
Dehya Qalbi <dehyasvibe@gmail.com>
2024-04-20 19:44:06 +0700

    add api resource route

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        \App\Models\Todo::create([
+            'title' => 'Test todo',
+            'is_done' => 0
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('todos', TodoController::class);
